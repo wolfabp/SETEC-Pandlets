1056fac23dbd3ad2c36a7801c280dc94fe02d34d
wolfabp <abpintor@hotmail.com>
2015-12-30 00:29:56 +0000

    didnt wanted this folder here

--- sample5/config/twi_master_config.h
+++
@@ -1,9 +0,0 @@
-#include "ble_services_config/ambient_service_config.h"
-
-#ifndef TWI_MASTER_CONFIG_H__
-#define TWI_MASTER_CONFIG_H__
-
-#include "board_config.h"
-
-#define TWI_MASTER_READ_DELAY             4//us
-#endif /* TWI_MASTER_CONFIG_H__ */
